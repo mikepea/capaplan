8816cba2d9ddce7509b8533bdc8ada1835fe117a
Mike Pountney <Mike.Pountney@gmail.com>
2013-10-01 22:45:01 +0100

    add nginx-only test gatline scenario, with JAVA_OPT overrides

---
+++ gatling/user-files/simulations/ProjectNginxDirectStaticFile.scala
@@ -0,0 +1,37 @@
+
+import com.excilys.ebi.gatling.core.Predef._
+import com.excilys.ebi.gatling.http.Predef._
+import com.excilys.ebi.gatling.jdbc.Predef._
+import com.excilys.ebi.gatling.http.Headers.Names._
+import akka.util.duration._
+import bootstrap._
+import assertions._
+
+class ProjectNginxDirectStaticFile extends Simulation {
+
+        // set via JAVA_OPTS="-Dfoo=bar"
+        val baseURL = System.getProperty("base_url")
+        val nbUsers = Integer.getInteger("users", 1)
+        val myRamp  = java.lang.Long.getLong("ramp", 0L)
+
+	val httpConf = httpConfig
+			.baseURL("http://icdplay02a.m53.eu")
+			.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+			.acceptEncodingHeader("gzip, deflate")
+			.acceptLanguageHeader("en-US,en;q=0.5")
+			.connection("keep-alive")
+			.userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:23.0) Gecko/20100101 Firefox/23.0")
+
+
+	val headers_2 = Map(
+			"Accept" -> """image/png,image/*;q=0.8,*/*;q=0.5"""
+	)
+
+
+	val scn = scenario("Scenario Name")
+		.exec(http("request_1")
+					.get("/test.html")
+			)
+
+	setUp(scn.users(nbUsers).ramp(myRamp).protocolConfig(httpConf))
+}
