e2e9de98571d086b9ef993f72c2b03464be2eedb
Mike Pountney <Mike.Pountney@gmail.com>
2013-10-06 00:44:26 +0100

    add test for db writing

---
+++ gatling/user-files/simulations/ProjectCapaplanLoadDBWrite.scala
@@ -0,0 +1,37 @@
+
+import com.excilys.ebi.gatling.core.Predef._
+import com.excilys.ebi.gatling.http.Predef._
+import com.excilys.ebi.gatling.jdbc.Predef._
+import com.excilys.ebi.gatling.http.Headers.Names._
+import akka.util.duration._
+import bootstrap._
+import assertions._
+
+class ProjectCapaplanLoadDBWrite extends Simulation {
+
+        // set via JAVA_OPTS="-Dfoo=bar"
+        val baseURL = System.getProperty("base_url")
+        val nbUsers = Integer.getInteger("users", 1)
+        val myRamp  = java.lang.Long.getLong("ramp", 0L)
+
+	val httpConf = httpConfig
+			.baseURL(baseURL)
+			.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+			.acceptEncodingHeader("gzip, deflate")
+			.acceptLanguageHeader("en-US,en;q=0.5")
+			.connection("keep-alive")
+			.userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:23.0) Gecko/20100101 Firefox/23.0")
+
+
+	val headers_2 = Map(
+			"Accept" -> """image/png,image/*;q=0.8,*/*;q=0.5"""
+	)
+
+
+	val scn = scenario("Scenario Name")
+		.exec(http("request_1")
+		.get("/load/db/write")
+	)
+
+	setUp(scn.users(nbUsers).ramp(myRamp).protocolConfig(httpConf))
+}
